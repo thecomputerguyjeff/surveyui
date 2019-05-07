b85a0bfe5a6bc54d00c06280906ffa748b375b77
Malka Bachrach <malka.bachrach@ti.htc.edu>
2019-05-07 15:32:13 -0500

    With fetch

--- src/App.js
+++ src/App.js
@@ -34,9 +34,39 @@ class App extends React.Component {
         )
     }
     //fetch("url").then(res=>{res.json().then(body=>{)},err=>{});
-    getByTitle = (title) => {
-        fetch('www.surveysurver.com/getByTitle/'+title).then(
-            res.json()).then(res=>this.setState({surveys: res}))
+    getByTitleEX = (title) => {
+        fetch("https://ti-survey-server.herokuapp.com/api/getShellByTitle/" + title)
+            .then((res) => res.json()
+            , (err)=>{
+                console.log("There is an error connecting to the website")
+                console.log(err)
+            })
+            .then((res) => {
+                console.log("success")
+                this.setState({ surveys: res })
+                debugger
+            }
+            , (err) => {
+                console.log("There was an error converting to json")
+                console.log(err)
+            })
+    }
+
+    getByAuthor = (author) => {
+        fetch("https://ti-survey-server.herokuapp.com/api/getShellByAuthor/" + author)
+            .then((res) => res.json()
+            ,(err)=>{
+                console.log("There was an error connecting to the website")
+                console.log(err)})
+            .then((res) => {
+                console.log("success")
+                this.setState({ surveys: res })
+                debugger
+            }
+            , (err) => {
+                console.log("There is an error converting to json")
+                console.log(err)
+            })
     }
 
 }
